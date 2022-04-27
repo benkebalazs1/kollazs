bf3610c14c3b6bad4039ab2119c2969df28b3ff1
benkebalazs1 <91192728+benkebalazs1@users.noreply.github.com>
2022-04-27 08:50:07 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,25 @@
+pepe1125.github.io/9c_web/kosar.html
+
+van plakat01 = 2000
+van plakat02 = 1500
+van plakat03 = :
+
+function hozzaad(termek) {
+    db = Number (prompt("db?"))
+    localStorage.setItem(termek, db)
+}
+
+ function TÖRLÉS() {
+    localStorage.clear()
+}
+
+function KOSÁR() {
+    var list=""
+    var termek="localStorage"
+    list + ="<tr> <th> név:  </th> <th>db</th> <th>ár</th> </tr>"
+    list + ="<tr> <td>"+termék+"</td>"
+}
+
+document.getElementById('KOSÁR').innerHTML = list { 
+
+}
